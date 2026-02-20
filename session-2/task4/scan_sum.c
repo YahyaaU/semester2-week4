bd1823ab7ce3e790a1f481a5feb69c626ef23c22
YahyaaU <glvd0689@leeds.ac.uk>
2026-02-20 12:50:34 +0000

    Task 4 completed but need to try fgets() and sscanf()

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -8,5 +8,17 @@ int main( void ) {
     // use scanf to read from the terminal
     // print the output from scanf and the data values 
 
+    // program using scan f
+
+    int a;
+    int b;
+    
+    printf("Enter 2 numbers which will be added together in the form a b: \n");
+    scanf("%d %d", &a, &b);
+
+    int c = a + b;
+
+    printf("%d + %d = %d\n", a, b, c);
+
     return 0;
 }

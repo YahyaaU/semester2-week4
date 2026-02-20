48e97dad6e55160e2dafb9e98dbc1ab7c63b04a8
YahyaaU <glvd0689@leeds.ac.uk>
2026-02-20 11:35:06 +0000

    Session 2 task 1

--- session-2/task1/not_strings.c
+++ session-2/task1/not_strings.c
@@ -1,16 +1,18 @@
 
 #include <stdio.h>
+#include <string.h>
 
 int main( void ) {
 
     char charArray[5] = {'h','e','l','l','o'};
     char charString[] = "hello";
  
-    printf("As array: %s\n",charArray);     // strange behaviour?
-    printf("Length of array: %ld\n",sizeof(charArray));   // expected result
+    printf("As array: %s\n", charArray);     // strange behaviour?
+    printf("Length of array: %ld\n", sizeof(charArray));   // expected result
 
     printf("As string: %s\n",charString);   // normal behaviour
-    printf("Length of string: %ld\n",sizeof(charString));   // includes silent character \O
+    printf("Length of string: %ld\n", sizeof(charString));   // includes silent character \O
 
+    printf("Real String Length: %ld\n", strlen(charString)); // Real size of the string using strlen
     return 0;
 }

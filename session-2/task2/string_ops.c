e3af6cbbe08ea1eeb778480900ac943186b6b5e3
YahyaaU <glvd0689@leeds.ac.uk>
2026-02-20 12:01:15 +0000

    Working with string functions

--- session-2/task2/string_ops.c
+++ session-2/task2/string_ops.c
@@ -14,10 +14,22 @@ int main( void ) {
     // use printf to verify the following tests
 
     // use 'strcmp' to compare 2 strings - print the return value
+    int cmp = strcmp(str1, str2);
+
+    printf("Comparing %s with %s: \n", str1, str2);
+    printf("strcmp returns %d\n\n", cmp);
 
     // use 'strcat' to concatenate 2 strings - print the resulting string
 
-    // use 'strcpy' to copy a string into str3 - print string 3
+    strcat(str1, str2);
+
+    printf("New string: %s\n", str1);
+
+    // use 'strcpy' to copy a string into str3 - print string 3 (MAKE SURE THERE IS ENOUGH SPACE, MAKE THE STRING SIZE BIGGER IF NEEDED)
+
+    printf("Before string copy: str3 = %s\n", str3);
+    strcpy(str3, str2); // copies the contents of str2 into str3
+    printf("After strcopy: str3 = %s\n", str3);
 
     return 0;
 }

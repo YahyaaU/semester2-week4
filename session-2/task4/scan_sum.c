d8611993685a7d883f2fc02fa0140d0e47e6128f
YahyaaU <glvd0689@leeds.ac.uk>
2026-02-20 22:41:44 +0000

    Completed scanf and sscanf and fgets

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -10,7 +10,7 @@ int main( void ) {
 
     // program using scan f
 
-    int a;
+    /*int a;
     int b;
     
     printf("Enter 2 numbers which will be added together in the form a b: \n");
@@ -18,7 +18,41 @@ int main( void ) {
 
     int c = a + b;
 
-    printf("%d + %d = %d\n", a, b, c);
+    printf("%d + %d = %d\n", a, b, c);*/
+
+    /*int i, j, k;
+    int count = scanf("%d %d %d", &i, &j, &k);
+
+    printf("%d\n", count);*/
+
+    /*char name[20];
+    printf("Enter your name: ");
+    fgets(name, 20, stdin);
+    printf("Your name is: %s\n", name);*/
+
+    
+    /*char input[20];
+    int age;
+
+    printf("Enter your age: ");
+    fgets(input, 20, stdin);
+
+    sscanf(input, "%d", &age);
+
+    printf("Your age is %d\n", age);*/
+
+    char user[20];
+    float temp;
+    float fahrenheit;
+
+    printf("Enter the temperature in celsius: ");
+    fgets(user, 20, stdin);
+
+    sscanf(user, "%f", &temp);
+
+    fahrenheit = (temp * 9/5) + 32;
+
+    printf("This is the temperature in fahrenheit: %.2f\n", fahrenheit);
 
     return 0;
 }
